cbc57558725969e6c76d44e40384d9a4a5812212
Alejandro Vázquez <vazquezale82@gmail.com>
2026-06-16 12:37:20 -0300

    Remove ScreenGlitch

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,9 @@
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
-import { ScreenGlitch } from "../ScreenGlitch/ScreenGlitch";
 
 export function Layout({ children }) {
   return (
     <>
-      <ScreenGlitch />
       <Header />
       <main className="main-content">{children}</main>
       <Footer />
